aba0f3b4ca7626755ac7ce5f5a0cfa497708000a
diwoka <diwoka@gmail.com>
2018-04-17 16:31:26 +0200

    debug

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,7 +9,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
@@ -18,18 +17,15 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends Controller
 {
     /**
-     * @param Request             $request
      * @param AuthenticationUtils $authUtils
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function loginAction(Request $request, AuthenticationUtils $authUtils)
+    public function loginAction(AuthenticationUtils $authUtils)
     {
         $error = $authUtils->getLastAuthenticationError();
 
         $lastUsername = $authUtils->getLastUsername();
-
-        $request;
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
