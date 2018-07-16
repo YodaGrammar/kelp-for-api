83a360eeb392bd2f774811b6e6edf22efb73ca8a
Diwoka <diwoka@gmail.com>
2018-07-16 16:43:11 +0200

    fix #134 correction date de peremption

---
+++ src/Migrations/Version20180716141713.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180716141713 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE kelp_product CHANGE date_peremption date_peremption DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE kelp_user CHANGE roles roles JSON NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE kelp_product CHANGE date_peremption date_peremption DATETIME DEFAULT NOT NULL');
+        $this->addSql('ALTER TABLE kelp_user CHANGE roles roles LONGTEXT NOT NULL COLLATE utf8mb4_bin');
+    }
+}
