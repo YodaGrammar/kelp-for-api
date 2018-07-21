e2a9f36fc4a683118b1cb49af7084dc1c77328d6
Florent DESPIERRES <florent@despierres.pro>
2018-07-21 12:03:42 +0200

    fixup: Move registration user login in FormHandler

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Form\Handler\RegistrationFormHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * Class RegistrationController.
@@ -14,11 +15,14 @@ class RegistrationController extends Controller
     /**
      * @param Request $request
      * @param RegistrationFormHandler $formHandler
+     * @param TranslatorInterface $translator
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function registerAction(Request $request, RegistrationFormHandler $formHandler)
+    public function registerAction(Request $request, RegistrationFormHandler $formHandler, TranslatorInterface $translator)
     {
         if ($formHandler->process($request)) {
+            $this->addFlash('success', $translator->trans('register.form.flash_message.success'));
+
             return $this->redirectToRoute('kelp.home');
         }
 
