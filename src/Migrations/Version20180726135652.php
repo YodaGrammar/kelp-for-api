e462bf2f04c1c6128eabfbbb9b72c258c88bfcae
Florent DESPIERRES <734582+orions@users.noreply.github.com>
2018-07-26 16:06:06 +0200

    Fix Expiration Date (#155)

---
+++ src/Migrations/Version20180726135652.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180726135652 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE kelp_product ADD expiration_date DATETIME DEFAULT NULL, DROP date_peremption');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE kelp_product ADD date_peremption DATETIME DEFAULT \'NULL\', DROP expiration_date');
+    }
+}
